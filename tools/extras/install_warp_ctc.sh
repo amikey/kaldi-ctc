0ab9f6ef79a25a1cafb3656aa6e08e3d62cda988
Feiteng <feiteng@liulishuo.com>
2016-10-07 14:13:20 +0800

    config warp-ctc

---
+++ tools/extras/install_warp_ctc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# install warp-ctc
+
+! which cmake >/dev/null  && \
+   echo "cmake is not installed, this will not work.  Ask your sysadmin to install it" && exit 1;
+
+if [ ! -d warp-ctc ];then
+	git clone git@github.com:lifeiteng/warp-ctc.git || exit 1
+fi
+
+cd warp-ctc; git pull
+mkdir -p build; cd build; cmake ../; make -j1 || exit 1
+./test_cpu || exit 1;
+./test_gpu || exit 1;
+cd ../../
